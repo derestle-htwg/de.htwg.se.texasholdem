e4bd9d609e12bbe5d733895576a84671fa5924a8
Dennis <Hawk30100@192.168.192.53>
2015-10-21 19:16:21 +0200

    Card-Klasse erweitert
    + UnitTests
    + toString-Methoden für Enums
    + toString-Methode für Card-Klasse

--- de.htwg.se.texasholdem/src/model/Card.java
+++ de.htwg.se.texasholdem/src/model/Card.java
@@ -1,69 +1,122 @@
 package model;
 
-import model.Card.Rank;
-import model.Card.Suit;
-
 public class Card {
 
-	Rank rank;
-	Suit suit;
-	
-	public Card(Rank rank, Suit suit) {
-		this.rank = rank;
-		this.suit = suit;
-	}
-
-	public static enum Suit {
-		S, D, H, C;	//Space, Diamond, Heart, Club
-	}
-	
 	public static enum Rank {
 		TWO {
+			@Override
 			public String toString() {
 				return "2";
 			}
 		},
 		THREE {
+			@Override
 			public String toString() {
 				return "3";
 			}
 		},
 		FOUR {
+			@Override
 			public String toString() {
 				return "4";
 			}
 		},
 		FIVE {
+			@Override
 			public String toString() {
 				return "5";
 			}
 		},
 		SIX {
+			@Override
 			public String toString() {
 				return "6";
 			}
 		},
 		SEVEN {
+			@Override
 			public String toString() {
 				return "7";
 			}
 		},
 		EIGHT {
+			@Override
 			public String toString() {
 				return "8";
 			}
 		},
 		NINE {
+			@Override
 			public String toString() {
 				return "9";
 			}
 		},
 		TEN {
+			@Override
 			public String toString() {
 				return "10";
 			}
 		},
-		J, Q, K, A
+		JACK {
+			@Override
+			public String toString() {
+				return "J";
+			}
+		},
+		QUEEN {
+			@Override
+			public String toString() {
+				return "Q";
+			}
+		},
+		KING {
+			@Override
+			public String toString() {
+				return "K";
+			}
+		},
+		ACE {
+			@Override
+			public String toString() {
+				return "A";
+			}
+		}
+	}
+
+	public static enum Suit {
+		SPACE {
+			@Override
+			public String toString() {
+				return "S";
+			}
+		},
+		DIAMOND {
+			@Override
+			public String toString() {
+				return "D";
+			}
+		},
+		HEART {
+			@Override
+			public String toString() {
+				return "H";
+			}
+		},
+		CLUB {
+			@Override
+			public String toString() {
+				return "C";
+			}
+		};
+	}
+
+	Rank rank;
+
+	Suit suit;
+
+	public Card(Rank rank, Suit suit) {
+		this.rank = rank;
+		this.suit = suit;
 	}
 
 	public Rank getRank() {
@@ -73,37 +126,12 @@ public class Card {
 	public Suit getSuit() {
 		return this.suit;
 	}
-	
-	/*private Suit suit;
-	private Rank rank;
-	
-	
-	public Card(Rank rank, Suit suit)
-	{
-		this.rank = rank;
-		this.suit = suit;
-	}
-	
-	public Rank getRank()
-	{
-		return rank;
-	}
-	
-	public Suit getSuit()
-	{
-		return suit;
-	}
-	
-	
+
 	@Override
-	public String toString()
-	{
+	public String toString() {
+
 		int cardNameMaxLength = 3;
-		StringBuilder s = new StringBuilder (suit + " " + rank);
-		for (@SuppressWarnings("unused") int i = 0; s.length() < cardNameMaxLength; i++)
-		{
-			s.append(" ");
-		}
+		StringBuilder s = new StringBuilder(suit + " " + rank);
 		return s.toString();
-	}*/
+	}
 }
--- de.htwg.se.texasholdem/test/model/CardTest.java
+++ de.htwg.se.texasholdem/test/model/CardTest.java
@@ -1,30 +1,48 @@
 package model;
 
-import static org.junit.Assert.*;
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class CardTest {
 
-	Card c1;
-	
+	Card c1, c2, c3, c4;
+
 	@Before
-	public void setup() {
-		
-	}
-	
-	@Test
-	public void getRank_inputHeartThree_returnsThree(){
-		c1 = new Card(Card.Rank.THREE, Card.Suit.H);
-		Assert.assertEquals(Card.Rank.THREE, c1.getRank());
+	public void _setup() {
+		c1 = new Card(Card.Rank.ACE, Card.Suit.DIAMOND);
+		c2 = new Card(Card.Rank.THREE, Card.Suit.HEART);
+		c3 = new Card(Card.Rank.KING, Card.Suit.CLUB);
+		c4 = new Card(Card.Rank.NINE, Card.Suit.SPACE);
 	}
 
 	@Test
-	public void getSuit_inputDiamondAce_returnsDiamond(){
-		c1 = new Card(Card.Rank.A, Card.Suit.D);
-		Assert.assertEquals(Card.Suit.D, c1.getSuit());
+	public void getRank_inputHeartThree_returnsThree() {
+		Assert.assertEquals(Card.Rank.THREE, c2.getRank());
 	}
-	
+
+	@Test
+	public void getSuit_inputDiamondAce_returnsDiamond() {
+		Assert.assertEquals(Card.Suit.DIAMOND, c1.getSuit());
+	}
+
+	@Test
+	public void toString_inputClubKing_returnsClubKingString() {
+		Assert.assertEquals("C K", c3.toString());
+	}
+
+	@Test
+	public void toString_inputDiamondAce_returnsDiamondAceString() {
+		Assert.assertEquals("D A", c1.toString());
+	}
+
+	@Test
+	public void toString_inputHeartThree_returnsHeartThreeString() {
+		Assert.assertEquals("H 3", c2.toString());
+	}
+
+	@Test
+	public void toString_inputSpaceNine_returnsSpaceNineString() {
+		Assert.assertEquals("S 9", c4.toString());
+	}
 }
