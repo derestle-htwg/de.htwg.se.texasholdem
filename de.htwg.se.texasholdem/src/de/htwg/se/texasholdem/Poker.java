a4a6d1e6864c7fe83f715077b12b8a4c145ebfe2
Dennis <debleich@htwg-konstanz.de>
2015-11-04 14:45:38 +0100

    Kommentare zu Poker-Klasse hinzugefügt

--- de.htwg.se.texasholdem/src/de/htwg/se/texasholdem/Poker.java
+++ de.htwg.se.texasholdem/src/de/htwg/se/texasholdem/Poker.java
@@ -2,4 +2,24 @@ package de.htwg.se.texasholdem;
 
 public class Poker {
 
+	// PokerController erstellen
+	// Erstelle TableManager
+	// Erstelle andere Manager
+
+	// Starte TUI / GUI
+
+	// Wie viele Spieler sollen an den Tisch?
+	// z.B. 4 Stück
+
+	// Wie sollen die heißen?
+	// z.B. "Hans"
+
+	// TUI -> PokerController.addPlayer("Hans")
+	// -> TableManager.addPlayer("Hans")
+	// -> PlayerManager.addPlayer("Hans")
+	// Hans hinzugefügt
+
+	// "SPIELEN"-Button gedrückt
+
+	// ...
 }
