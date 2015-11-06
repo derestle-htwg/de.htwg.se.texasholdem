6e2f0d0601be34c05f37ea750933b5dca33e3f33
vgoh <vandicleiago@hotmail.com>
2015-11-06 11:36:28 +0100

    Umwandlung des Projekts in Maven Project: Im POM File: Java	Compiler Configuriert 					JUnit Dependency hinzugefuegt

---
+++ de.htwg.se.texasholdem/src/de/htwg/se/texasholdem/controller/GameMaster.java
@@ -0,0 +1,12 @@
+package de.htwg.se.texasholdem.controller;
+
+/**
+ * Der Gamemaster Steuert das Spiel.
+ * 	er verteilt Karten,
+ * 	er beachtet die Regeln
+ * 	er Fragt die Mitglieder nach ihren Zügen
+ * */
+
+public class GameMaster {
+
+}
