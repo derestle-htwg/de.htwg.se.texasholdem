3b471aa5d9b43165385733cb48a30b4973e2036b
debleich <debleich@htwg-konstanz.de>
2015-12-12 15:54:38 +0100

    Deleted GameManager

--- de.htwg.se.texasholdem/src/de/htwg/se/texasholdem/controller/PokerController.java
+++ de.htwg.se.texasholdem/src/de/htwg/se/texasholdem/controller/PokerController.java
@@ -1,9 +1,27 @@
 package de.htwg.se.texasholdem.controller;
 
+import java.util.List;
+
+import de.htwg.se.texasholdem.model.Player;
 import de.htwg.se.texasholdem.util.observer.IObservable;
 
 public interface PokerController extends IObservable {
 
 	String getTableString();
 
+	void addPlayer(Player player);
+
+	void clearActivePlayers();
+
+	List<Player> getActivePlayers();
+
+	List<Player> getPlayerList();
+
+	Player getStartPlayer();
+
+	void setPlayerActive(Player player);
+
+	void setPlayerNotActive(Player player);
+
+	void setStartPlayer();
 }
--- de.htwg.se.texasholdem/src/de/htwg/se/texasholdem/controller/imp/PokerControllerImp.java
+++ de.htwg.se.texasholdem/src/de/htwg/se/texasholdem/controller/imp/PokerControllerImp.java
@@ -1,18 +1,24 @@
 package de.htwg.se.texasholdem.controller.imp;
 
-import de.htwg.se.texasholdem.controller.GameManager;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
 import de.htwg.se.texasholdem.controller.ModelManager;
 import de.htwg.se.texasholdem.controller.PokerController;
+import de.htwg.se.texasholdem.model.Player;
 import de.htwg.se.texasholdem.util.observer.Observable;
 
 public class PokerControllerImp extends Observable implements PokerController {
 
+	private Player startPlayer;
 	private ModelManager modelManager;
-	private GameManager gameManager;
+	private List<Player> activePlayers;
+	private Player currentPlayer;
 
 	public PokerControllerImp() {
 		modelManager = new ModelManagerImp();
-		gameManager = new GameManagerImp(modelManager);
+		this.activePlayers = new LinkedList<Player>();
 	}
 
 	public String getTableString() {
@@ -23,4 +29,36 @@ public class PokerControllerImp extends Observable implements PokerController {
 
 	}
 
+	public void addPlayer(Player player) {
+		modelManager.addPlayer(player);
+	}
+
+	public void clearActivePlayers() {
+		activePlayers.clear();
+	}
+
+	public List<Player> getActivePlayers() {
+		return activePlayers;
+	}
+
+	public List<Player> getPlayerList() {
+		return modelManager.getPlayerList();
+	}
+
+	public Player getStartPlayer() {
+		return startPlayer;
+	}
+
+	public void setPlayerActive(Player player) {
+		activePlayers.add(player);
+	}
+
+	public void setPlayerNotActive(Player player) {
+		activePlayers.remove(player);
+	}
+
+	public void setStartPlayer() {
+		int randomNumber = ThreadLocalRandom.current().nextInt(0, modelManager.getPlayerList().size());
+		startPlayer = modelManager.getPlayerList().get(randomNumber);
+	}
 }
